62f3ab3e8e3db7e04501cbcca1e406bfb12649e4
pchellovod <pchellovod@gmail.com>
2017-05-16 19:29:31 +0500

    add link to positions

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :positions
 
   get 'welcome/index'
-
+  get '/employees/position/:position_id', controller: 'employees', action: 'for_position', as: 'for_position'
   root'welcome#index'
 end
