68609ed54fd3bd87c9342d91a5a137562009202e
pchellovod <pchellovod@gmail.com>
2017-05-17 13:34:47 +0500

    fix validates

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,21 +1,22 @@
 class Employee < ActiveRecord::Base
   validates :name, presence: true, length: {maximum: 255}, uniqueness: true
   validates :position, presence: true
-  validate  :name_check, :date_of_birth_cannot_be_in_the_past
+  validates :name, format: { with: /(\w+)\s+(\w+)\s+(\w+)/, message: 'field will contain name, surname and lastname'}
+  validate  :date_of_birth_cannot_be_in_the_future# :name_check, 
 
   belongs_to :position
 
-  def name_check
-    name = :name.to_s.split
-    if name.count == 3
-      redirect_to @employee, notice: 'Record was successfully created.'
-    else
-      errors.add(:name, "field will contain name, surname and lastname")
-    end
-  end
+  # def name_check
+  #   name = :name.to_s.split
+  #   if name.count == 3
+  #     redirect_to @employee, notice: 'Record was successfully created.'
+  #   else
+  #     errors.add(:name, "field will contain name, surname and lastname")
+  #   end
+  # end
 
-  def date_of_birth_cannot_be_in_the_past
-    if dateofbirth.parse('2010-11-01') > Time.now
+  def date_of_birth_cannot_be_in_the_future
+    if dateofbirth > Time.now
       errors.add(:dateofbirth, "can't be in the future")
     end
   end
