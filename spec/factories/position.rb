527c116e446c19976f0bb1abccbde6d67ac9282c
pchellovod <pchellovod@gmail.com>
2017-05-18 15:48:40 +0500

    add tests for position_controller

---
+++ spec/factories/position.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :position do
+    title "MyString"
+  end  
+end
--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,22 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
+  position = Position.create(title: "Engineer")
+  let(:position) { create(:position) }
+  
   it 'validates presence of name' do
-    expect(Employee.new(name: '',dateofbirth: "#{Date.today - rand(40).years}")).to_not be_valid
+    expect(Employee.new(name: '',dateofbirth: "#{Date.today - rand(40).years}", position: position)).to_not be_valid
   end
 
   it 'validates maximum length of name' do
     expect(Employee.new(name: "#{SecureRandom.base64(257)}",
-                        dateofbirth: "#{Date.today - rand(40).years}")).to_not be_valid
+                        dateofbirth: "#{Date.today - rand(40).years}", position: position)).to_not be_valid
   end
 
   it 'validates date can not be in the future' do
     expect(Employee.new(name: "#{SecureRandom.base64(57)}",
-                        dateofbirth: "#{Date.today + 1.years}")).to_not be_valid
+                        dateofbirth: "#{Date.today + 1.years}", position: position)).to_not be_valid
   end
 
   it 'validates name to contain three words' do
-    expect(Employee.new(name: "dfassfdsfd asdfasfd asdfasfdsadf",
-                        dateofbirth: "#{Date.today - rand(40).years}")).to_not be_valid
+    expect(Employee.new(name: "dfass sadfsdf ",
+                        dateofbirth: "#{Date.today - rand(40).years}", position: position)).to be_valid
   end
 end
